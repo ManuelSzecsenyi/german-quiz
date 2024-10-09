566c41ffb2e521b7eadb6ecdf178cd8a1af54106
Manuel Szecsenyi <manuel.szecsenyi@gmail.com>
2024-10-09 21:37:59 +0200

    Fixes logo

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -31,7 +31,7 @@
                     <a href="/" class="-m-1.5 p-1.5">
                         <span class="sr-only">Your Company</span>
                         <img class="h-8 w-auto"
-                             src="{{url('/gladlio-logo.svg')}}" alt="">
+                             src="{{asset('storage/gladlio-logo.svg')}}" alt="">
                     </a>
                 </div>
                 @if (Route::has('login'))
